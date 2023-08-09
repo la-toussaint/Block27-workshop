748bf2b6a5b932023e8a16efe8d9c912aaf227a9
Lauren August Toussaint <96217271+la-glitch@users.noreply.github.com>
2023-08-09 14:50:43 -0400

    collab w/ KT - Sign-Up compon complete

--- workshop/src/App.jsx
+++ workshop/src/App.jsx
@@ -1,10 +1,11 @@
-import './App.css'
-
+import Authenticate from "./Authenticate.jsx";
+import SignUpForm from "./SignUpForm.jsx";
 export default function App() {
   return (
     <>
-
+      <Authenticate />
+      <SignUpForm />
     </>
-  )
+  );
 }
 
